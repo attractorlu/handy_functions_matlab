f7e1bb351498f4305ac641b627b29d221c7d8a3e
LiangShe <liangsher@gmail.com>
2018-02-27 17:53:30 -0800

    imporve the input for edge smoothness

--- image/im_replace_subregion.m
+++ image/im_replace_subregion.m
@@ -1,4 +1,4 @@
-function [ im ] = im_replace_subregion( im, bg, mask)
+function [ im ] = im_replace_subregion( im, bg, mask, sigma)
 % im  = im_replace_subregion( im, bg, mask)
 % replace subregion of image with background 
 % background will be scaled to the same size as image
@@ -8,12 +8,15 @@ function [ im ] = im_replace_subregion( im, bg, mask)
 %     bg: background
 %     mask: binary array as same size of image, 
 %                1 mark pixels to keep, 0 to replace
-
-
-% sigma: sigma of Gaussian smooth in pixel
-sigma = 10;
+%     sigma: sigma of Gaussian smooth in pixel
 
 [y, x] = size(im(:,:,1));
+
+if nargin<4
+    sigma = y*0.05;
+end
+
+
 bg = imresize(bg, [y x]);
 
 im = im_mix(im, bg, mask, sigma);
