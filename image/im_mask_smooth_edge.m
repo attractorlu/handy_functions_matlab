93790be312200264de229752e759a12301e88305
LiangShe <liangsher@gmail.com>
2018-03-15 10:09:30 -0700

    a few minor improvements

--- image/im_mask_smooth_edge.m
+++ image/im_mask_smooth_edge.m
@@ -8,21 +8,21 @@ function [ im ] = im_mask_smooth_edge( im, mask, sigma)
 %     sigma: sigma of Gaussian smooth in pixel
 %
 
+[y, x] = size(im(:,:,1));
+
 if nargin<3
-    sigma = 10;
+    sigma = y*0.005;
 end
 
-[y, x] = size(im(:,:,1));
-[xx, yy] = meshgrid(1:x,1:y);
+bg = 128;
 
 if ~islogical(mask)
     % if it's not bool, it should be a polygon.
+    [xx, yy] = meshgrid(1:x,1:y);
     in  = inpolygon(xx(:), yy(:), mask(:,1), mask(:,2));
     mask = reshape(in, y, x);
 end
 
-% figure; imagesc(mask)
- 
 mask = double(mask);
 mask = imgaussfilt(mask, sigma);
 
@@ -30,9 +30,5 @@ mask = (mask - 0.5)*2;
 mask(mask<0 ) = 0;
 
 % figure; imagesc(mask)
-
-
-for i = 1:size(im,3)
-    im(:,:,i) = double(im(:,:,i)).* mask + (1-mask)*128;
-end
+im = im_mix(im, bg, mask);
 
