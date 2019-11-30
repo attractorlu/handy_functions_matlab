2b8b42f37313119c8671a1e19c8482f7dda0e1f1
LiangShe <liangsher@gmail.com>
2019-11-29 20:36:11 -0800

    more flexible calling options for error_area

--- visualization/error_area.m
+++ visualization/error_area.m
@@ -1,14 +1,44 @@
-function error_area( x, y, e, c )
+function error_area( varargin )
+% error_area( y, e ) 
+% error_area( y, e, c ) 
 % error_area( x, y, e, c ) 
 
-x = x(:);
-y = y(:);
-e = e(:);
+if nargin == 1
+    error('Not enough input arguments')
+    
+elseif nargin <= 3
+    y = varargin{1}(:);
+    e = varargin{2}(:);
+    x = (1:numel(y))';
+    if nargin == 3
+        c = varargin{3};
+    else
+        c = [];
+    end
+    
+elseif nargin == 4
+    x = varargin{1}(:);
+    y = varargin{2}(:);
+    e = varargin{3}(:);
+    c = varargin{4};
+
+end
 
 upper = y+e;
 lower = y-e;
 err_y = [upper; lower(end:-1:1)];
 err_x = [x; x(end:-1:1)];
-patch('XData', err_x, 'YData', err_y, 'FaceColor', c, 'EdgeColor', c);
+
+if isempty(c)
+    patch('XData', err_x, 'YData', err_y, 'EdgeColor', 'none');
+    hold on
+    plot(x, y, 'k')
+else
+    patch('XData', err_x, 'YData', err_y, 'FaceColor', c, 'EdgeColor', 'none');
+    hold on
+    plot(x, y, 'Color', c)
+end
+
+alpha(0.5)
 
 end
