a57b92465684dfcae5911f1ba35c0da1039e65d5
LiangShe <liangsher@gmail.com>
2022-06-02 23:51:46 -0700

    Update findn.m

--- analysis/findn.m
+++ analysis/findn.m
@@ -1,10 +1,10 @@
 function ind = findn( data, data_to_find )
 %findn 
 n = length(data_to_find);
-ind = [];
+ind = nan(n,1);
 for i=1:n
     k = find(data==data_to_find(i));
-    ind = [ind k];
+    ind(i) = k;
 end
 
 end
